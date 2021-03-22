c8be1b30b2d6d4814b2867563a63dffd91ac07b3
simonhettrick <s.hettrick@software.ac.uk>
2021-03-22 15:55:56 +0000

    Coding style improvements

--- inflammation/models.py
+++ inflammation/models.py
@@ -15,7 +15,8 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+    """Calculate the daily mean of a 2d inflammation data array.
+    """
     return np.mean(data, axis=0)
 
 
